c10664a264cc3889a1ef2149b30440ff8022c1e4
stiff.roy <stiff.roy@gmail.com>
2020-04-19 05:31:01 +0200

    [TASK] Truffle config changes

--- truffle.js
+++ truffle.js
@@ -5,15 +5,10 @@ module.exports = {
   networks: {
     development: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:9545/", 0, 50);
       },
       network_id: '*',
       gas: 9999999
-    }
-  },
-  compilers: {
-    solc: {
-      version: "^0.4.24"
     }
   }
 };
