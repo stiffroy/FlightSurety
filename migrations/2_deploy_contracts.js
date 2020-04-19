4773c54cc5474735df5fb429412e051c782e28f4
stiff.roy <stiff.roy@gmail.com>
2020-04-19 05:31:23 +0200

    [TASK] Update the deployment process

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,15 +3,14 @@ const FlightSuretyData = artifacts.require("FlightSuretyData");
 const fs = require('fs');
 
 module.exports = function(deployer) {
-
     let firstAirline = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
-    deployer.deploy(FlightSuretyData)
-    .then(() => {
-        return deployer.deploy(FlightSuretyApp)
+    deployer.deploy(FlightSuretyData, firstAirline)
+        .then(() => {
+            return deployer.deploy(FlightSuretyApp, FlightSuretyData.address)
                 .then(() => {
                     let config = {
                         localhost: {
-                            url: 'http://localhost:8545',
+                            url: 'http://localhost:9545',
                             dataAddress: FlightSuretyData.address,
                             appAddress: FlightSuretyApp.address
                         }
@@ -19,5 +18,5 @@ module.exports = function(deployer) {
                     fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
                     fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
                 });
-    });
+        });
 }
