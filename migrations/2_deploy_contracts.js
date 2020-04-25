638cedf3b16b21b09a141d90ae0e97fe97f33813
stiff.roy <stiff.roy@gmail.com>
2020-04-25 22:23:06 +0200

    [REFACTOR] Removing and resetting files after deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,14 +5,6 @@ const fs = require('fs');
 module.exports = function(deployer) {
     let firstAirline = '0x00b8FB19C75a0cF4cF57977eD05200f81Fa70b1c';
 
-    if (fs.existsSync(__dirname + '/../src/dapp/config.json')) {
-        fs.unlinkSync(__dirname + '/../src/dapp/config.json');
-    }
-
-    if (fs.existsSync(__dirname + '/../src/server/config.json')) {
-        fs.unlinkSync(__dirname + '/../src/server/config.json');
-    }
-
     deployer.deploy(FlightSuretyData, firstAirline)
         .then(() => {
             return deployer.deploy(FlightSuretyApp, FlightSuretyData.address)
@@ -23,6 +15,13 @@ module.exports = function(deployer) {
                             dataAddress: FlightSuretyData.address,
                             appAddress: FlightSuretyApp.address
                         }
+                    }
+                    if (fs.existsSync(__dirname + '/../src/dapp/config.json')) {
+                        fs.unlinkSync(__dirname + '/../src/dapp/config.json');
+                    }
+
+                    if (fs.existsSync(__dirname + '/../src/server/config.json')) {
+                        fs.unlinkSync(__dirname + '/../src/server/config.json');
                     }
                     fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
                     fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
